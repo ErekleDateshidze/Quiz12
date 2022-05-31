5a61f4a1554dcf2fac4a6b289c5a1979a7dba8b0
Erekle99 <erekledateshidze99@gmail.com>
2022-05-31 15:00:50 +0400

    1

---
+++ js.js
@@ -0,0 +1,55 @@
+country = {
+    name: "შვეიცარია",
+    languages: ["გერმანული", "ფრანგული", "იტალიური"],
+    capital: {
+        name: "ბერნი",
+        population: 123123
+    },
+    cities: [
+        { name: "ცურიხი", population: 31232131 },
+        { name: "ჟენევა", population: 12313 },
+        { name: "რამე", population: 13123211231 }
+    ]
+}
+
+document.write("შვეიცარიის ოფიციალური ენებია" + ": ");
+for (var i = 0; i < country.languages.length; i++) {
+    document.write(country.languages[i] + " ");
+}
+
+document.write("</br>" + "შვეიცარიის დედაქალაქია" + " :");
+document.write(country.capital.name + " " + country.capital.population)
+
+document.write("</br>" + "ქალაქები" + ": ");
+for (var i = 0; i < country.cities.length; i++) {
+    document.write(country.cities[i].name + " " + country.cities[i].population + " ");
+}
+
+
+film = {
+    name: "Cavea Cinemas",
+    languages: ["English", "Russian", "Georgian"],
+    premiere: {
+        name: "Cavea City Mall",
+        attendance: 100000
+    },
+    movies: [
+        { name: "Top Gun Maverick", imdb: 9.2 },
+        { name: "Doctor Strange", imdb: 8.7 },
+        { name: "Batman", imdb: 6.2 }
+    ]
+}
+
+
+document.write("Language Options" + ":");
+for (var i = 0; i < film.languages.length; i++) {
+    document.write(film.languages[i] + " ");
+}
+
+document.write("</br>" + "Grand Premiere" + ":");
+document.write(film.premiere.name + " " + film.premiere.attendance);
+
+document.write("</br>" + "Movies" + ":");
+for (var i = 0; i < film.movies.length; i++) {
+    document.write(film.movies[i].name + "-" + film.movies[i].imdb + " ");
+}
